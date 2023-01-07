5960fcbfca2249e3a3dc24b88c07bac83c996572
NotKilleration <77356224+NotKilleration@users.noreply.github.com>
2023-01-07 21:34:18 +0530

    hangman final commit

--- main.py
+++ main.py
@@ -1,9 +1,101 @@
+import random
 #Step 1 
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+chosen_word = random.choice(word_list)
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+chances = 6
+
+print(f"Testing: {chosen_word}")
+
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+
+
+
+
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+        
+
+    print(display)
+
+    if guess not in chosen_word:
+        print("Wrong guess!")
+        chances = chances - 1
+        if chances == 0:
+            end_of_game = True
+            print("Hanged. GAME OVER")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You Win!")
+
+    print(stages[chances])
